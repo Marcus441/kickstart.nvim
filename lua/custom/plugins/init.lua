6120a3492eedc7e39a98060e20be9c09d0fec85d
Marcus441 <psminchia4153@hotmail.com>
2024-08-18 21:15:09 +1000

    added nvchad plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { import = 'plugins.spec.autosession' },
+  { import = 'plugins.spec.harpoon' },
+  { import = 'plugins.spec.vim-tmux-navigator' },
+  { import = 'plugins.spec.nvim-java' },
+  { import = 'plugins.spec.ts-autotag' },
+  { import = 'plugins.override.mason' },
+  { import = 'plugins.override.nvim-treesitter' },
+  { import = 'plugins.override.lspconfig' },
+  { import = 'plugins.override.conform' },
+}
