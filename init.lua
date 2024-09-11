2030f13b00d500285e789d5959bcfc8a73586cce
Marcus441 <psminchia4153@hotmail.com>
2024-09-12 09:10:57 +1000

    fix: removed tsserver

--- init.lua
+++ init.lua
@@ -529,8 +529,15 @@ require('lazy').setup({
         pyright = {},
         tailwindcss = {},
         yamlls = {},
-        tsserver = {},
-        rust_analyzer = {},
+        rust_analyzer = {
+          settings = {
+            ['rust_analyzer'] = {
+              cargo = {
+                allFeatures = true,
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
