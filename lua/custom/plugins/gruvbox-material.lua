99551ea6db8645eeaed377bf0dcd85af11e4437f
Marcus441 <psminchia4153@hotmail.com>
2024-10-12 09:51:38 +1000

    style: make background transparent

--- lua/custom/plugins/gruvbox-material.lua
+++ lua/custom/plugins/gruvbox-material.lua
@@ -9,6 +9,7 @@ return {
     vim.g.gruvbox_material_foreground = 'material'
     vim.g.gruvbox_material_better_performance = 1
     vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_transparent_background = 2
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     vim.cmd.colorscheme 'gruvbox-material'
